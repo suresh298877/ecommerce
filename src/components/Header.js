2470ceb3ba05c8570d13adfa66b998744c52ad85
suresh298877 <sureshpeddimsetti298@gmail.com>
2023-11-12 11:37:23 +0530

    lasdjfk

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ function Header() {
                     <Link className="navbar-brand" to="/">MBU Cart</Link>
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="/ecommerce/">Home</Link>
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/categories">Categories</Link>
